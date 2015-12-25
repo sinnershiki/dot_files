131b3940f204ad9901f152ab7a9754938fb7a1cc
Kousuke Sugano <sinner.shiki.g@gmail.com>
2015-12-25 14:48:59 +0900

    first commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,241 @@
+;;---------------------------------------------------
+;;
+;; El-get設定
+;;---------------------------------------------------
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+;; el-get https://github.com/dimitri/el-get
+(add-to-list 'load-path (locate-user-emacs-file "el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+;; el-getでダウンロードしたパッケージは ~/.emacs.d/ に入るようにする
+(setq el-get-dir (locate-user-emacs-file "elisp"))
+
+;;---------------------------------------------------
+;;
+;; Emacs全般の設定
+;;---------------------------------------------------
+;; 起動時に出てくるメッセージを消す
+(setq inhibit-startup-message t)
+
+;; 行、列の表示
+(line-number-mode t)
+(column-number-mode t)
+(global-linum-mode t)
+
+;; C-h to Backspace
+(global-set-key "\C-h" 'delete-backward-char)
+
+;; C-c C-c to comment-or-uncomment-region
+(global-set-key "\C-c\C-c" 'comment-or-uncomment-region)
+
+;; タブ幅を設定
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; ビープを無効に
+(setq ring-bell-function 'ignore)
+
+;; 折り返し
+(setq truncate-partial-width-windows nil)
+
+;; シンボリックリンクのリンク先を直接開く
+(setq vc-follow-symlinks t)
+
+;; ファイルの自動リロード
+(global-auto-revert-mode 1)
+
+;; ツールバー・メニューバーの非表示
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+;; スクロールを良い感じにする
+(defun scroll-down-with-lines ()
+  "" (interactive) (scroll-down 3))
+(defun scroll-up-with-lines ()
+  "" (interactive) (scroll-up 3))
+(global-set-key [wheel-up] 'scroll-down-with-lines)
+(global-set-key [wheel-down] 'scroll-up-with-lines)
+(global-set-key [double-wheel-up] 'scroll-down-with-lines)
+(global-set-key [double-wheel-down] 'scroll-up-with-lines)
+(global-set-key [triple-wheel-up] 'scroll-down-with-lines)
+(global-set-key [triple-wheel-down] 'scroll-up-with-lines)
+
+;; 保存するタイミングでdelete-trailing-whitespace
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; リージョン削除
+(delete-selection-mode t)
+
+;;----------------------------------------------------
+;;
+;; Text and Font
+;;----------------------------------------------------
+;; 背景色と文字色
+(setq default-frame-alist
+      (append
+       '((foreground-color . "ivory")
+         (background-color . "black")
+         (cursor-color . "deep pink")
+         (alpha . (80 55 nil nil)))
+       default-frame-alist))
+
+;; フォントを指定
+(set-face-attribute 'default nil
+                    :family "Ricty"
+                    :height 180)
+
+;;---------------------------------------------------
+;;
+;; 機能拡張
+;;---------------------------------------------------
+;;画面を 2 分割したときの 上下を入れ替える swap screen
+(defun swap-screen()
+  "Swap two screen,leaving cursor at current window."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (nextbuf (window-buffer (next-window))))
+    (set-window-buffer (next-window) (window-buffer))
+    (set-window-buffer thiswin nextbuf)))
+(defun swap-screen-with-cursor()
+  "Swap two screen,with cursor in same buffer."
+  (interactive)
+  (let ((thiswin (selected-window))
+        (thisbuf (window-buffer)))
+    (other-window 1)
+    (set-window-buffer thiswin (window-buffer))
+    (set-window-buffer (selected-window) thisbuf)))
+(global-set-key [f2] 'swap-screen)
+(global-set-key [S-f2] 'swap-screen-with-cursor)
+
+;; 対応する括弧をハイライト
+(show-paren-mode t)
+(setq show-paren-style 'expression)
+(set-face-background 'show-paren-match-face "gray15")
+(set-face-foreground 'show-paren-match-face "white")
+
+;; 矢印キーで画面移動
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+
+(put 'upcase-region 'disabled nil)
+
+;;---------------------------------------------------
+;;
+;; el-getによるPackage管理
+;;---------------------------------------------------
+;; anzu設定
+(el-get-bundle anzu)
+(global-anzu-mode +1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-search-threshold 1000)
+ '(safe-local-variable-values
+   (quote
+    ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
+           (add-hook
+            (quote write-contents-functions)
+            (lambda nil
+              (delete-trailing-whitespace)
+              nil))
+           (require
+            (quote whitespace))
+           "Sometimes the mode needs to be toggled off and on."
+           (whitespace-mode 0)
+           (whitespace-mode 1))
+     (whitespace-line-column . 80)
+     (whitespace-style face tabs trailing lines-tail)))))
+
+;; autocomplete
+(el-get-bundle auto-complete)
+(ac-config-default)
+
+;;yasnippet設定
+(el-get-bundle yasnippet)
+(yas-global-mode 1)
+
+;; ruby-mode
+(el-get-bundle enh-ruby-mode)
+(el-get-bundle ruby-block)
+(el-get-bundle ruby-electric)
+(add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+;; 括弧の後のインデントが深くなるのを防ぐ
+(setq ruby-deep-indent-paren-style nil)
+;; マジックコメントの自動挿入を停止
+(setq ruby-insert-encoding-magic-comment nil)
+(add-hook 'enh-ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+(setq ruby-electric-expand-delimiters-list nil)
+;; ruby-block.el --- highlight matching block
+(require 'ruby-block)
+(ruby-block-mode t)
+(setq ruby-block-highlight-toggle t)
+
+;; rhtml-mode
+(el-get-bundle rhtml-mode)
+(add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
+
+;; haml-mode
+(el-get-bundle haml-mode)
+
+;; haskell-mode
+(el-get-bundle haskell-mode)
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
+
+;; yaml-mode
+(el-get-bundle yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+;; coffee-mode
+(el-get-bundle coffee-mode)
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (and (set (make-local-variable 'tab-width) 2)
+       (set (make-local-variable 'coffee-tab-width) 2))
+  )
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
+;; json-mode
+(el-get-bundle json-mode)
+(add-hook 'json-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 2)))
+
+;; git
+(el-get-bundle git-gutter)
+(el-get-bundle magit)
+
+;; twittering mode
+(el-get-bundle twittering-mode)
+(setq twittering-use-master-password t)
+
+;; ess (R)
+;;(el-get-bundle ess)
+;;(el-get-bundle ess-R-data-view)
+;;(setq auto-mode-alist
+;;     (cons (cons "\\.r$" 'R-mode) auto-mode-alist))
+;;(autoload 'R-mode "ess-site" "Emacs Speaks Statistics mode" t)
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; )
---
+++ .zshrc
@@ -0,0 +1,200 @@
+########################################
+# 環境変数
+export LANG=ja_JP.UTF-8
+
+# 色を使用出来るようにする
+autoload -Uz colors
+colors
+
+# emacs 風キーバインドにする
+bindkey -e
+
+# ヒストリの設定
+HISTFILE=~/.zsh_history
+HISTSIZE=1000000
+SAVEHIST=1000000
+
+# 単語の区切り文字を指定する
+autoload -Uz select-word-style
+select-word-style default
+# ここで指定した文字は単語区切りとみなされる
+# / も区切りと扱うので、^W でディレクトリ１つ分を削除できる
+zstyle ':zle:*' word-chars " /=;@:{},|"
+zstyle ':zle:*' word-style unspecified
+
+########################################
+# 補完
+# 補完機能を有効にする
+autoload -Uz compinit
+compinit
+
+# 補完で小文字でも大文字にマッチさせる
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# ../ の後は今いるディレクトリを補完しない
+zstyle ':completion:*' ignore-parents parent pwd ..
+
+# sudo の後ろでコマンド名を補完する
+zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin \
+    /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin
+
+# ps コマンドのプロセス名補完
+zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
+
+########################################
+# vcs_info
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' formats '(%s)-[%b]'
+zstyle ':vcs_info:*' actionformats '(%s)-[%b|%a]'
+precmd () {
+    psvar=()
+    LANG=en_US.UTF-8 vcs_info
+    [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
+}
+RPROMPT="%1(v|%F{green}%1v%f|)"
+
+########################################
+# オプション
+# 日本語ファイル名を表示可能にする
+setopt print_eight_bit
+
+# beep を無効にする
+setopt no_beep
+
+# フローコントロールを無効にする
+setopt no_flow_control
+
+# '#' 以降をコメントとして扱う
+setopt interactive_comments
+
+# ディレクトリ名だけでcdする
+setopt auto_cd
+
+# cd したら自動的にpushdする
+setopt auto_pushd
+
+# 重複したディレクトリを追加しない
+setopt pushd_ignore_dups
+
+# = の後はパス名として補完する
+setopt magic_equal_subst
+
+# 同時に起動したzshの間でヒストリを共有する
+setopt share_history
+
+# 同じコマンドをヒストリに残さない
+setopt hist_ignore_all_dups
+
+# ヒストリファイルに保存するとき、すでに重複したコマンドがあったら古い方を削除する
+setopt hist_save_nodups
+
+# スペースから始まるコマンド行はヒストリに残さない
+setopt hist_ignore_space
+
+# ヒストリに保存するときに余分なスペースを削除する
+setopt hist_reduce_blanks
+
+# 補完候補が複数あるときに自動的に一覧表示する
+setopt auto_menu
+
+# 高機能なワイルドカード展開を使用する
+setopt extended_glob
+
+########################################
+# キーバインド
+# ^R で履歴検索をするときに * でワイルドカードを使用出来るようにする
+bindkey '^R' history-incremental-pattern-search-backward
+
+########################################
+# エイリアス
+alias la='ls -a'
+alias ll='ls -l'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# sudo の後のコマンドでエイリアスを有効にする
+alias sudo='sudo '
+
+# グローバルエイリアス
+alias -g L='| less'
+alias -g G='| grep'
+
+########################################
+# 言語
+#rbenv
+export PATH=$HOME/.rbenv/bin:$PATH
+eval "$(rbenv init - zsh)"
+
+########################################
+#tmux
+autoload -Uz add-zsh-hook
+function rename_tmux_window() {
+   if [ $TERM = "screen" ]; then
+       local current_path=`pwd | sed -e s/\ /_/g`
+       local current_dir=`basename $current_path`
+       tmux rename-window $current_dir
+   fi
+}
+add-zsh-hook precmd rename_tmux_window
+
+########################################
+# OS 別の設定
+case ${OSTYPE} in
+    darwin*)
+        #Mac用の設定
+        # プロンプト
+        PROMPT="%{${fg[red]}%}${bg[white]}%}[%n@%m]%{${reset_color}%}
+%~%# "
+
+        export CLICOLOR=1
+        export LSCOLORS=gxfxcxdxbxegedabagacad
+        alias ls='ls -G -F'
+
+        # emacs
+        alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
+
+        #vscode
+        vscode () {
+            VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $*
+        }
+
+        #tex
+        export PATH=$PATH:/usr/local/texlive/2014/bin/x86_64-darwin
+
+        # Go PATH
+        export GOPATH=$HOME/go
+        export PATH=$PATH:$GOPATH/bin
+
+        # MySQL Path Setting
+        export PATH=$PATH:/usr/local/mysql/bin
+
+        # virtualenvwrapperが使うpythonを指定
+        export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+        export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+        # pipをグローバルにインストールした場合でも, virtualenv環境にパッケージをインストールする.
+        export PIP_RESPECT_VIRTUALENV=true
+        source "/usr/local/bin/virtualenvwrapper.sh"
+
+        #nodejs
+        #export NODE_PATH=/usr/local/lib/node_modules
+        #export PATH=$PATH:$NODE_PATH
+
+        ### Added by the Heroku Toolbelt
+        export PATH="/usr/local/heroku/bin:$PATH"
+
+        #mosquitto
+        alias mosquitto_pub=/usr/local/bin/mosquitto_pub
+        alias mosquitto_sub=/usr/local/bin/mosquitto_sub
+        ;;
+    linux*)
+        #Linux用の設定
+        # プロンプト
+        PROMPT="%{${fg[red]}%}[%n@%m]%{${reset_color}%}
+%~%# "
+
+        #ls 色付け
+        alias ls='ls -F --color'
+        ;;
+esac
