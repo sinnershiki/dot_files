9d24fee0c6b888d9027985ab16a7a42cd7cd9146
Kousuke Sugano <sinner.shiki.g@gmail.com>
2016-11-28 13:54:48 +0900

    google翻訳el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -147,6 +147,45 @@
 
 ;; 便利系
 ;;---------------------------------------------------
+;; popwin
+(el-get-bundle popwin)
+(setq display-buffer-function 'popwin:display-buffer)
+(setq popwin:popup-window-position 'bottom)
+
+;; google transelate
+(el-get-bundle google-translate)
+(require 'google-translate-default-ui)
+(defvar google-translate-english-chars "[:ascii:]’“”–"
+  "これらの文字が含まれているときは英語とみなす")
+(defun google-translate-enja-or-jaen (&optional string)
+  "regionか、現在のセンテンスを言語自動判別でGoogle翻訳する。"
+  (interactive)
+  (setq string
+        (cond ((stringp string) string)
+              (current-prefix-arg
+               (read-string "Google Translate: "))
+              ((use-region-p)
+               (buffer-substring (region-beginning) (region-end)))
+              (t
+               (save-excursion
+                 (let (s)
+                   (forward-char 1)
+                   (backward-sentence)
+                   (setq s (point))
+                   (forward-sentence)
+                   (buffer-substring s (point)))))))
+  (let* ((asciip (string-match
+                  (format "\\`[%s]+\\'" google-translate-english-chars)
+                  string)))
+    (run-at-time 0.1 nil 'deactivate-mark)
+    (google-translate-translate
+     (if asciip "en" "ja")
+     (if asciip "ja" "en")
+     string)))
+(global-set-key (kbd "C-c t") 'google-translate-enja-or-jaen)
+;; google-translate.elの翻訳バッファをポップアップで表示させる
+(push '("*Google Translate*") popwin:special-display-config)
+
 ;; anzu設定
 (el-get-bundle anzu)
 (global-anzu-mode +1)
@@ -232,6 +271,7 @@
 
 ;; quickrun
 (el-get-bundle quickrun)
+(push '("*quickrun*") popwin:special-display-config)
 (global-set-key (kbd "<f5>") 'quickrun)
 (global-set-key (kbd "C-<f5>") 'quickrun-with-arg)
 (global-set-key (kbd "M-<f5>") 'quickrun-compile-only)
