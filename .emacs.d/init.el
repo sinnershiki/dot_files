5ac35cf30212f1ea621ad6197d6d63c69748ad2f
Kousuke Sugano <sinner.shiki.g@gmail.com>
2016-12-04 22:09:57 +0900

    YaTeX-modeのprefixをC-tに変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -455,6 +455,7 @@
 (setq auto-mode-alist
       (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
 (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+(setq YaTeX-prefix "\C-t")
 (setq tex-command "platex")
 (setq dviprint-command-format "dvipdfmx %s")
 ;; use utf-8 on yatex mode
